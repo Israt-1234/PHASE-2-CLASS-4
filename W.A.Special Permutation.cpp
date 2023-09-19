0ee7911b6767520f61c8442b925fcb3cb7ed25f9
Israt jahan <140290748+Israt-1234@users.noreply.github.com>
2023-09-19 20:20:33 +0600

    Add files via upload

---
+++ W.A.Special Permutation.cpp
@@ -0,0 +1,16 @@
+//https://codeforces.com/problemset/problem/1454/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n; cin>>n;
+    while(n--){
+        int t; cin>>t;
+        cout<<t<<" ";
+        for(int i=1;i<t;i++)
+            cout<<i<<" ";
+            cout<<endl;
+    }
+return 0;
+}
