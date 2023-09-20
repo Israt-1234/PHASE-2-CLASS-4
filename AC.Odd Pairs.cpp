3f8c54a1d5a6fec47a4c0190b6efb5472fdacf6e
Israt jahan <140290748+Israt-1234@users.noreply.github.com>
2023-09-20 13:19:01 +0600

    Add files via upload

---
+++ AC.Odd Pairs.cpp
@@ -0,0 +1,14 @@
+//https://www.codechef.com/problems/ODDPAIRS
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    int t; cin>>t;
+    while(t--){
+       ll n;cin>>n;
+        ll sum=(n*n)/2;
+        cout<<sum<<'\n';
+    }
+    return 0;
+}
